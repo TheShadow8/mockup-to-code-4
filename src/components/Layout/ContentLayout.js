497fe26003f5bbf93b03dda55bb611b6fae28b9b
TheShadow8 <minhdo.ca@gmail.com>
2019-01-01 20:24:11 -0500

    add media query

--- src/components/Layout/ContentLayout.js
+++ src/components/Layout/ContentLayout.js
@@ -5,4 +5,11 @@ export default styled.div`
   grid-column: 1/2;
   font-size: 1.6rem;
   display: flex;
+
+  @media (max-width: 50em) {
+    height: auto;
+    grid-column: auto;
+    grid-row: 1 / 2;
+    flex-direction: column;
+  }
 `;
--- src/components/Layout/GalleryLayout.js
+++ src/components/Layout/GalleryLayout.js
@@ -3,4 +3,9 @@ import styled from 'styled-components';
 export default styled.div`
   height: 100%;
   grid-column: 2/3;
+
+  @media (max-width: 50em) {
+    grid-column: auto;
+    grid-row: 2 /3;
+  }
 `;
